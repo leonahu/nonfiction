a8f02e9908e7463b88f7fa5c87c7f7b36d03cdfe
Ricky Boyce <boycce@gmail.com>
2018-01-08 15:58:54 +1300

    visibility

--- site/views/projects.php
+++ site/views/projects.php
@@ -4,14 +4,13 @@
   $o->class = $page->slug();
   $o->title = b::title($page);
   $o->tree = c::get("tree");
-  $o->projects = $page->children();
   ob_start();
 ?>
 
 
 <main class="list">
   <div class="box">
-    <?php foreach ($page->children() as $s) {
+    <?php foreach ($page->children()->visible() as $s) {
       $cover = $s->coverimage()->toFile();
       $thumb = ($cover)? $cover->crop(662, 472) : null;
       $thumburl = ($cover)? $thumb->url() : '';
@@ -24,42 +23,6 @@
         <a href="/<?php echo $s->uri() ?>"><img src="<?php echo b::asset($thumburl) ?>"></a>
       </div>
     <?php } ?>
-    <!--
-    <div>
-      <div class="title">
-        <h2>Wireless Headphone</h2>
-        <h3>Human Inc</h3>
-      </div>
-      <a href="#"><img src="<?php echo b::asset($o->tree['imgs']['featured']['2']) ?>"></a>
-    </div>
-    <div>
-      <div class="title">
-        <h2>Shuttle Collection</h2>
-        <h3>Eternal Luxe x Divka</h3>
-      </div>
-      <a href="#"><img src="<?php echo b::asset($o->tree['imgs']['featured']['3']) ?>"></a>
-    </div>
-    <div>
-      <div class="title">
-        <h2>Wireless charging ecosystem</h2>
-        <h3>Intel</h3>
-      </div>
-      <a href="#"><img src="<?php echo b::asset($o->tree['imgs']['featured']['4']) ?>"></a>
-    </div>
-    <div>
-      <div class="title">
-        <h2>Vuli: Tremor Management Wearable</h2>
-        <h3>Cala Health</h3>
-      </div>
-      <a href="#"><img src="<?php echo b::asset($o->tree['imgs']['featured']['1']) ?>"></a>
-    </div>
-    <div>
-      <div class="title">
-        <h2>Wireless Headphone</h2>
-        <h3>Human Inc</h3>
-      </div>
-      <a href="#"><img src="<?php echo b::asset($o->tree['imgs']['featured']['2']) ?>"></a>
-    </div>-->
   </div>
 </main>
 
